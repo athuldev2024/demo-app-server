995c5dca2bbf7a2280f7ba73fec889b8fc08769b
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2024-10-16 00:18:06 +0530

    updated controller

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,131 +1,107 @@
-const JSONdb = require('simple-json-db');
-const { customAlphabet } = require('nanoid');
-const util = require('util');
-const createError = require('http-errors');
+const util = require("util");
+const createError = require("http-errors");
 
-const STATUS_CODES = require('../constants/status-codes');
-const MESSAGES = require('../constants/messages');
-
-const nanoid = customAlphabet('1234567890', 18);
-const DB_URL = './src/database/storage.db';
-const db = new JSONdb(DB_URL, {
-	asyncWrite: false,
-});
+const STATUS_CODES = require("../constants/status-codes");
+const MESSAGES = require("../constants/messages");
+const {
+  checkIfExistsInDB,
+  storeUserToDB,
+  loginUserDB,
+  deleteUserDB,
+  updateUserDB,
+} = require("../models/user-model");
 
 const registerUser = async (req, res, next) => {
-	try {
-		if (db.has(req.body.name)) {
-			return res
-				.status(STATUS_CODES.userExists)
-				.json({ message: MESSAGES.userExists });
-		}
+  try {
+    if (await checkIfExistsInDB(req.body.mobile)) {
+      return res
+        .status(STATUS_CODES.userExists)
+        .json({ message: MESSAGES.userExists });
+    }
 
-		db.set(req.body.name, JSON.stringify(req.body));
+    const userID = await storeUserToDB(req.body);
 
-		req.session.user = { userID: nanoid(), name: req.body.name };
+    req.session.user = { userID, name: req.body.name };
 
-		return res
-			.status(STATUS_CODES.userRegistered)
-			.json({ message: MESSAGES.userRegistered });
-	} catch (error) {
-		next(createError(error.statusCode, error.message));
-	}
+    return res
+      .status(STATUS_CODES.userRegistered)
+      .json({ message: MESSAGES.userRegistered });
+  } catch (error) {
+    next(createError(error.statusCode, error.message));
+  }
 };
 
 const loginUser = async (req, res, next) => {
-	try {
-		if (!db.has(req.body.name)) {
-			return res
-				.status(STATUS_CODES.noResource)
-				.json({ message: MESSAGES.noResource });
-		}
+  try {
+    if (!(await checkIfExistsInDB(req.body.mobile))) {
+      return res
+        .status(STATUS_CODES.noResource)
+        .json({ message: MESSAGES.noResource });
+    }
 
-		const userFromDb = JSON.parse(db.get(req.body.name));
+    const userID = await loginUserDB(req.body.mobile, req.body.password);
 
-		if (userFromDb.password !== req.body.password) {
-			return res
-				.status(STATUS_CODES.unAuthenticated)
-				.json({ message: MESSAGES.unAuthenticated });
-		}
+    if (!userID) {
+      return res
+        .status(STATUS_CODES.unAuthenticated)
+        .json({ message: MESSAGES.unAuthenticated });
+    }
 
-		// Setting the session
-		req.session.user = { userID: nanoid(), name: req.body.name };
+    req.session.user = { userID, name: req.body.name };
 
-		return res
-			.status(STATUS_CODES.success)
-			.json({ message: MESSAGES.loginSuccess });
-	} catch (error) {
-		next(createError(error.statusCode, error.message));
-	}
+    return res
+      .status(STATUS_CODES.success)
+      .json({ message: MESSAGES.loginSuccess });
+  } catch (error) {
+    next(createError(error.statusCode, error.message));
+  }
 };
 
 const logoutUser = async (req, res, next) => {
-	try {
-		const destroySession = util
-			.promisify(req.session.destroy)
-			.bind(req.session);
-		await destroySession();
+  try {
+    const destroySession = util
+      .promisify(req.session.destroy)
+      .bind(req.session);
+    await destroySession();
 
-		res.clearCookie('connect.sid');
+    res.clearCookie("connect.sid");
 
-		return res
-			.status(STATUS_CODES.success)
-			.json({ message: MESSAGES.logoutSuccess });
-	} catch (error) {
-		next(createError(error.statusCode, error.message));
-	}
-};
-
-const updateUser = async (req, res, next) => {
-	try {
-		if (!db.has(req.params.name)) {
-			return res
-				.status(STATUS_CODES.noResource)
-				.json({ message: MESSAGES.noResource });
-		}
-
-		db.set(
-			req.params.name,
-			JSON.stringify({ ...JSON.parse(db.get(req.params.name)), ...req.body })
-		);
-
-		return res
-			.status(STATUS_CODES.noContent)
-			.json({ message: MESSAGES.updated });
-	} catch (error) {
-		next(createError(error.statusCode, error.message));
-	}
+    return res
+      .status(STATUS_CODES.success)
+      .json({ message: MESSAGES.logoutSuccess });
+  } catch (error) {
+    next(createError(error.statusCode, error.message));
+  }
 };
 
 const deleteUser = async (req, res, next) => {
-	try {
-		if (!db.has(req.params.name)) {
-			return res
-				.status(STATUS_CODES.noResource)
-				.json({ message: MESSAGES.noResource });
-		}
+  try {
+    await deleteUserDB(req.params.userID);
 
-		const destroySession = util
-			.promisify(req.session.destroy)
-			.bind(req.session);
-		await destroySession();
+    return res
+      .status(STATUS_CODES.noContent)
+      .json({ message: MESSAGES.deleted });
+  } catch (error) {
+    next(createError(error.statusCode, error.message));
+  }
+};
 
-		res.clearCookie('connect.sid');
+const updateUser = async (req, res, next) => {
+  try {
+    await updateUserDB(req.body, req.params.userID);
 
-		db.delete(req.params.name);
-
-		return res
-			.status(STATUS_CODES.noContent)
-			.json({ message: MESSAGES.deleted });
-	} catch (error) {
-		next(createError(error.statusCode, error.message));
-	}
+    return res
+      .status(STATUS_CODES.noContent)
+      .json({ message: MESSAGES.updated });
+  } catch (error) {
+    next(createError(error.statusCode, error.message));
+  }
 };
 
 module.exports = {
-	registerUser,
-	loginUser,
-	deleteUser,
-	updateUser,
-	logoutUser,
+  registerUser,
+  loginUser,
+  deleteUser,
+  updateUser,
+  logoutUser,
 };

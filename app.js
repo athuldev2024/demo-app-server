c1a59906762b2488baf609998b0bb3bbe6a15085
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2024-11-03 17:31:20 +0530

    added cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const FileStore = require("session-file-store")(session);
 const swaggerUi = require("swagger-ui-express");
 const hbs = require("hbs");
 const swaggerDocs = require("./swagger");
+const cors = require("cors");
 
 const hbsRouter = require("./src/routes/hbs-routes");
 const usersRouter = require("./src/routes/user-routes");
@@ -42,6 +43,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "handlebars/public")));
